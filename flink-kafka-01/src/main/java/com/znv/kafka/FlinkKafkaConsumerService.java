d99ac987c676d1c225305cc04a7b33d6b021a6f0
wangrui <0049001950@znv.com>
2020-01-17 14:04:41 +0800

    自己编写了一个很简单的sink 验证流程

--- flink-kafka-01/src/main/java/com/znv/kafka/FlinkKafkaConsumerService.java
+++ flink-kafka-01/src/main/java/com/znv/kafka/FlinkKafkaConsumerService.java
@@ -1,12 +1,17 @@
 package com.znv.kafka;
 
+import org.apache.flink.api.common.io.OutputFormat;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
+import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -20,15 +25,33 @@ public class FlinkKafkaConsumerService {
         FlinkKafkaConsumer flinkKafkaConsumer = new FlinkKafkaConsumer("bap-etl-door-event", new SimpleStringSchema(), getProperties());
         flinkKafkaConsumer.setStartFromEarliest();
         DataStream ds = env.addSource(flinkKafkaConsumer);
-        ds.print();
+        ds.addSink(new JsonPrintSink());
         env.execute("kafka flink test");
     }
 
+    static class JsonPrintSink extends RichSinkFunction<String> {
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            System.out.println("sink open");
+        }
+
+        @Override
+        public void close() throws Exception {
+            System.out.println("sink close");
+        }
+
+        @Override
+        public void invoke(String value, Context context) {
+            System.out.println("one time");
+            System.out.println(value);
+        }
+    }
 
     public static Properties getProperties() {
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "face.dct-znv.com:9092");
-        properties.setProperty("group.id", "testflinkgroup1");
+        properties.setProperty("group.id", "testflinkgroup12");
         properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         return properties;
